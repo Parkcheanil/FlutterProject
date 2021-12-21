4a268227a48d403c91be0b78e8ab540a81a81ecf
Chunil Park <s10e01s@naver.com>
2021-12-21 23:10:38 +0900

    UI 박스 레이아웃 연습

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,20 +8,44 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar( title: Text('ddd'), ),
+        appBar: AppBar(
+          title: GestureDetector(
+            onTap: () {
+              print("click");
+            },
+            child: Row(
+              children: [
+                Text('아라동',
+                  style: TextStyle(color: Colors.black),
+                ),
+                Icon(Icons.arrow_drop_down),
+              ],
+            ),
+          ),
+          elevation: 1,
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.tune)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.tune)),
+          ],
+        ),
         body: Container(
           child: Row(
             children: [
-              Image.asset('dog.jpg'),
-
+              ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                child: Image.asset('assets/images/dog.jpg', width: 100, height: 100,),
+              ),
             ],
           ),
         ),
+        bottomNavigationBar: Container(),
       )
     );
   }
